1ba691ed0556f0479f05d35253a60b60a82ad6fc
Victor Deserto <victor.deserto@gmail.com>
2020-10-12 18:23:44 -0400

    First Commit

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {FiArrowRight} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
+import '../styles/pages/landing.css'
+
+import logoImg from '../images/logo.svg'
+
+function Landing() {
+    return (
+        <div id="page-landing">
+        <div className="content-wrapper">
+          <img src={logoImg} alt ="Happy"/>
+  
+            <main>
+              <h1>Leve a felicidade para o mundo</h1> 
+              <p>Visite orfanatos e mude o dia de outras crianças</p>
+            </main>
+  
+            <div className="location">
+                <strong>Campo Grande</strong>
+                <span>Mato Grosso do Sul</span>
+            </div>
+  
+            <Link to="/app" className="enter-app">
+              <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+  
+            </Link>
+        </div>
+      </div>
+    );
+}
+
+export default Landing;
